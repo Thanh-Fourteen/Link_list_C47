59598236f6491140a991cc96880654646270875e
Tony <thanhfourteen@gmail.com>
2026-01-27 21:43:10 +0700

    feat(menu): table menu

--- main.cpp
+++ main.cpp
@@ -1,18 +1,94 @@
 #include "insert.cpp"
 #include "delete.cpp"
+#include "color.h"
+
+void menu() {
+	printf("%s+----------------------- Menu -----------------------+%s\n", GREEN, RESET);
+	printf("%s| %-50s |%s\n", RED, "0. Exit", RESET);
+	printf("%s| %-50s |%s\n", CYAN, "1. Add to the beginning of the list", RESET);
+	printf("%s| %-50s |%s\n", CYAN, "2. Add to the end of the list", RESET);
+	printf("%s| %-50s |%s\n", CYAN, "3. Insert an element at a position", RESET);
+	printf("%s| %-50s |%s\n", CYAN, "4. Add an element after a specific element", RESET);
+	printf("%s| %-50s |%s\n", CYAN, "5. Add an element before a specific element", RESET);
+	printf("%s| %-50s |%s\n", CYAN, "6. Remove the first element", RESET);
+	printf("%s| %-50s |%s\n", CYAN, "7. Remove the last element", RESET);
+	printf("%s| %-50s |%s\n", CYAN, "8. Remove an element at a position", RESET);
+	printf("%s| %-50s |%s\n", CYAN, "9. Remove a specific element", RESET);
+	printf("%s| %-50s |%s\n", CYAN, "10. Remove an element after a specific element", RESET);
+	printf("%s| %-50s |%s\n", CYAN, "11. Remove an element before a specific element", RESET);
+	printf("%s| %-50s |%s\n", YELLOW, "12. Print the linked list", RESET);
+	printf("%s| %-50s |%s\n", YELLOW, "13. Load test cases from file", RESET);
+	printf("%s+----------------------------------------------------+%s\n", GREEN, RESET);
+}
 
 int main() {
+	menu();
+	int choice;
 	Node *head = NULL;
-	insertAtTail(&head, 3); 
-	insertAtTail(&head, 3);
-	insertAtTail(&head, 6);
-	insertAtTail(&head, 3);
-	insertAtTail(&head, 3);
-	insertAtTail(&head, 8);
+	do {
+		system("cls"); // Clear the screen
+		menu();
+		printf("Your choice: ");
+		scanf("%d", &choice);
+		int value, position, key, key_value;
+		char filename[100];
 
-	printList(head);	// 8 -> 3 -> 5 -> 6 -> 3 -> 6 -> NULL
-	deleteAllKeys(&head, 3); 
-	printList(head);    // 3 -> 5 -> 3 -> 6 -> NULL
+		switch (choice) {
+			case 1:
+				printf("Enter the value to add: ");
+				scanf("%d", &value);
+				insertAtHead(&head, value);
+				break;
+			case 2:
+				break;
+			case 3: // Insert at position
+				printf("Enter the position: ");
+				scanf("%d", &position);
+				printf("Enter the value to add: ");
+				scanf("%d", &value);
+				insertAtPosition(&head, value, position);
+				break;
+
+			case 4: // Add after a specific element
+				break;
+			case 5: // Add before a specific element
+				break;
+			case 6: // Delete the first element
+				deleteAtHead(&head);
+				printf("List after deleting the first element:\n");
+				printList(head);
+				system("pause");
+				break;
+
+			case 7: // Delete the last element
+				break;
+			case 8: // Delete at position
+				break;
+			case 9: // Delete all occurrences of a value
+				break;
+			case 10: // Delete after a specific element
+				break;
+			case 11: // Delete before a specific element
+				break;
+			case 12:
+				printf("Current linked list: ");
+				printList(head);
+				system("pause");
+				break;
+		
+			case 13: // Load test cases from file
+				break;
+			default:
+				printf("Error choice!\n");
+				system("pause");
+				break;
+		}
+
+	} while (choice != 0);
 
 	freeList(&head);
+	return 0;
 }
+
+
+
